8273b6e018867bea8cc38bd870c94701892f7d78
shikhar verma <130604353+shikharvermasv@users.noreply.github.com>
2024-04-03 23:20:06 +0530

    Create lowest-common-ancestor-of-a-binary-search-tree.cpp

---
+++ homework/trees 6/lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        int small = min(p->val, q->val);
+        int large = max(p->val, q->val);
+        while (root != NULL) {
+            if (root->val > large){
+                root = root->left;
+            }
+            else if (root->val < small){
+                root = root->right;
+            }
+            else{
+                return root;
+            }
+        }
+        return NULL;
+    }
+};
